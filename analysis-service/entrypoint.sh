4db758eee6c9b9c224414bff9729e029e97b0d1b
Alex <aagitolyev@kaaiot.io>
2019-09-25 13:04:59 +0300

    enhance: move Dockerfile for analysis service to corresponding dir, update docker-compose

---
+++ analysis-service/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DEFAULT_PRESIDIO_API_SVC_ADDRESS=presidio-api:8080
+
+if [ -z "$PRESIDIO_API_SVC_ADDRESS" ]; then
+  PRESIDIO_API_SVC_ADDRESS=$DEFAULT_PRESIDIO_API_SVC_ADDRESS
+fi
+
+# TODO: add api service polling as rediness probe
+sleep 30
+
+exec java -jar /usr/local/lib/analysis-service.jar
