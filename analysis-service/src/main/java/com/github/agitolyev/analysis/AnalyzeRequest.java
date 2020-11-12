8b722b394f0495ae0b316bf84bd10c519f2a8581
agitolyev <alexandr.ahitoliev@vgs.io>
2020-11-12 17:32:51 +0200

    enhance: stringify text

--- analysis-service/src/main/java/com/github/agitolyev/analysis/AnalyzeRequest.java
+++ analysis-service/src/main/java/com/github/agitolyev/analysis/AnalyzeRequest.java
@@ -1,6 +1,14 @@
 package com.github.agitolyev.analysis;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Collections;
+import java.util.Map;
+
 public class AnalyzeRequest {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     private static final String REQUEST_TEMPLATE = "{\"text\": \"%s\", \"analyzeTemplate\":{\"allFields\":true}}";
 
@@ -10,7 +18,36 @@ public class AnalyzeRequest {
         this.text = text;
     }
 
-    public String analyzeRequestJson() {
-        return String.format(REQUEST_TEMPLATE, text);
+    public String analyzeRequestJson() throws JsonProcessingException {
+        PresidioRequest request = new PresidioRequest(this.text, Collections.singletonMap("allFields", true));
+        return objectMapper.writeValueAsString(request);
+    }
+
+    private class PresidioRequest {
+
+        String text;
+
+        Map<String, Object> analyzeTemplate;
+
+        public PresidioRequest(String text, Map<String, Object> analyzeTemplate) {
+            this.text = text;
+            this.analyzeTemplate = analyzeTemplate;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
+
+        public Map<String, Object> getAnalyzeTemplate() {
+            return analyzeTemplate;
+        }
+
+        public void setAnalyzeTemplate(Map<String, Object> analyzeTemplate) {
+            this.analyzeTemplate = analyzeTemplate;
+        }
     }
 }
